77c528756c94b7627c74d729aec6a6a1eca82867
Pascal Friederich <paukul@gmail.com>
2010-11-08 18:07:04 +0100

    maybe, maybe not, dunno \/o\/

--- lib/amqp_logging/metrics_agent/rails.rb
+++ lib/amqp_logging/metrics_agent/rails.rb
@@ -1,6 +1,13 @@
 require 'amqp_logging'
 require 'amqp_logging/metrics_agent/rails/after_dispatch_callback_handler'
 
+class ActionController::Base
+  attr_writer :metrics_agent
+
+  def metrics_agent
+    @metrics_agent ||= AMQPLogging::MetricsAgent.new
+  end
+end
 
 ActionController::Dispatcher.after_dispatch do |dispatcher|
   AMQPLogging::Rails::AfterDispatchCallbackHandler.run(dispatcher)
@@ -8,7 +15,7 @@ end
 
 class ActionController::Base
   def log_processing_for_request_id_with_metrics_agent
-    agent[:page] = "#{self.class.name}\##{action_name}"
+    metrics_agent[:page] = "#{self.class.name}\##{action_name}"
     log_processing_for_request_id_without_metrics_agent
   end
   alias_method_chain :log_processing_for_request_id, :metrics_agent
