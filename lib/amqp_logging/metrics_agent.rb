a1b131c22118ba803b4bab5ae78a10a59b755886
Pascal Friederich <paukul@gmail.com>
2010-11-08 16:13:49 +0100

    take log level into account and allow different types of loggers

--- lib/amqp_logging/metrics_agent.rb
+++ lib/amqp_logging/metrics_agent.rb
@@ -1,36 +1,72 @@
+require 'json'
+
+
 module AMQPLogging
   class MetricsAgent
-    attr_reader :fields
-
     def initialize
       @default_fields = {
         :host => Socket.gethostname.split('.').first,
-        :process => Process.pid
-      }
-      @fields = {
+        :pid => Process.pid,
         :loglines => {
           :default => []
         }
       }
+      @logger_types = {}
+      reset_fields
     end
 
-    def add_logline(severity, message, progname)
-      t = Time.now
-      @fields[:loglines][:default] << [severity, t.strftime("%d.%m.%YT%H:%M:%S.#{t.usec}"), message || progname]
+    def logger
+      @logger || (self.logger = ::Logger.new($stdout))
     end
-    
-    def wrap_logger(logger)
+
+    def logger=(logger)
+      @logger = logger
+      @logger.formatter = Proc.new {|_, _, msg, progname| msg || progname}
+      @logger
+    end
+
+    def flush
+      logger.info(@fields.to_json)
+    end
+
+    def [](fieldname)
+      @fields[fieldname]
+    end
+
+    def []=(fieldname, value)
+      @fields[fieldname] = value
+    end
+
+    def add_logline(severity, message, progname, logger)
+      t = Time.now
+      @fields[:loglines][@logger_types[logger]] << [severity, t.strftime("%d.%m.%YT%H:%M:%S.#{t.usec}"), message || progname]
+    end
+
+    def wrap_logger(logger, type=:default)
       agent = self
+      register_logger(logger, type)
       logger.instance_eval do
         @agent = agent
         alias :add_without_proxy :add
         def add_with_proxy(severity, message, progname)
-          @agent.add_logline(severity, message, progname)
+          @agent.add_logline(severity, message, progname, self) unless severity < @level
           add_without_proxy(severity, message, progname)
         end
         alias :add :add_with_proxy
       end
       logger
+    end
+
+    private
+    def register_logger(logger, type)
+      @logger_types[logger] = type
+      @fields[:loglines][type] = []
+    end
+
+    def reset_fields
+      @fields = {
+      }.merge!(@default_fields)
+      @logger_types.values.each {|logtype| @fields[:loglines][logtype] = []}
     end
   end
 end
--- test/metrics_agent_test.rb
+++ test/metrics_agent_test.rb
@@ -1,8 +1,38 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
+
 module AMQPLogging
-  # class MetricsAgentTest
-  # end
+  class MetricsAgentTest < Test::Unit::TestCase
+    def setup
+      @agent = MetricsAgent.new
+      @out = StringIO.new
+      @agent.logger = Logger.new(@out)
+    end
+    
+    test "should record the process id" do
+      assert_equal Process.pid, @agent[:pid]
+    end
+    
+    test "should record the hostname" do
+      assert_equal Socket.gethostname.split('.').first, @agent[:host]
+    end
+    
+    test "should have convenience methods for accessing the fields" do
+      @agent[:foo] = :bar
+      assert_equal :bar, @agent[:foo]
+      assert_equal @agent[:foo], @agent[:foo]
+    end
+    
+    test "should send the collected data as json when flushed" do
+      @agent.flush
+      json = JSON.parse(@out.string)
+      assert_equal Process.pid, json["pid"]
+    end
+    
+    test "should reset the collected data when flushed" do
+      
+    end
+  end
 
   class LoggingProxyTest < Test::Unit::TestCase
     def setup
@@ -17,14 +47,39 @@ module AMQPLogging
     end
 
     test "should register every logline on the agent" do
-      @agent.expects(:add_logline).with(0, nil, "foobar")
+      @agent.expects(:add_logline).with(0, nil, "foobar", @logger)
       @proxy.debug("foobar")
     end
 
+    test "should take the loglevel of the logger into account" do
+      @logger.level = ::Logger::INFO
+      no_lines_before_logging = @agent[:loglines][:default].size
+      @logger.debug "something"
+      assert_equal no_lines_before_logging, @agent[:loglines][:default].size
+    end
+
     test "should store the loglines" do
-      assert_equal 0, @agent.fields[:loglines][:default].size
+      assert_equal 0, @agent[:loglines][:default].size
       @proxy.debug("foobar")
-      assert_equal 1, @agent.fields[:loglines][:default].size
+      assert_equal 1, @agent[:loglines][:default].size
+    end
+
+    test "should store each logline with severity, a timestamp and the message" do
+      some_logline = "asdf0asdf"
+      @proxy.debug "foo"
+      @proxy.warn  "bar"
+      @proxy.info  some_logline
+      severity, timestamp, message = @agent[:loglines][:default][2]
+      assert_equal Logger::INFO, severity
+      assert_nothing_raised { Time.parse(timestamp) }
+      assert_equal some_logline, message
+    end
+    
+    test "should allow to register multiple loggers with different types" do
+      other_logger = ::Logger.new('/dev/null')
+      @agent.wrap_logger(other_logger, :sql)
+      other_logger.info("some fancy stuff here")
+      assert_equal 1, @agent[:loglines][:sql].size
     end
   end
 end
