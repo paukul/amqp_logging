1225ffdfdb7c1766c48a4d22580c6febc63058b0
Pascal Friederich <paukul@gmail.com>
2010-11-11 14:52:53 +0100

    ISO date ftw

--- lib/amqp_logging/metrics_agent/rails.rb
+++ lib/amqp_logging/metrics_agent/rails.rb
@@ -9,7 +9,7 @@ class ActionController::Base
   def log_processing_for_request_id_with_metrics_agent
     logger.agent[:page] = "#{self.class.name}\##{action_name}"
     t = Time.now
-    logger.agent[:started_at] = t.strftime("%d.%m.%YT%H:%M:%S.#{t.usec}")
+    logger.agent[:started_at] = t.strftime("%Y.%d.%mT%H:%M:%S.#{t.usec}")
     log_processing_for_request_id_without_metrics_agent
   end
   alias_method_chain :log_processing_for_request_id, :metrics_agent
