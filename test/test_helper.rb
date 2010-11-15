de3e71ac54efc28369afc0ba14ccb2f9bac1250b
Pascal Friederich <paukul@gmail.com>
2010-11-15 15:19:29 +0100

    don't require amqp_logging.rb twice in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,6 @@ require 'test/unit'
 require 'mocha'
 require 'active_support/testing/declarative'
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/amqp_logging')
-
 begin
   require 'redgreen' unless ENV['TM_FILENAME']
 rescue LoadError
