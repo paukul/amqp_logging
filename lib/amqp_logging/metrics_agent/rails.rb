352cb044b60629aae1d036e24598468a959ef860
Pascal Friederich <pascal.friederich@xing.com>
2011-08-08 14:44:06 +0200

    logjam compatibility change

--- lib/amqp_logging/metrics_agent/rails.rb
+++ lib/amqp_logging/metrics_agent/rails.rb
@@ -7,7 +7,7 @@ end
 
 class ActionController::Base
   def log_processing_for_request_id_with_metrics_agent
-    logger.agent[:page] = "#{self.class.name}\##{action_name}"
+    logger.agent[:action] = "#{self.class.name}\##{action_name}"
     logger.agent[:started_at] = AMQPLogging.iso_time_with_microseconds
     log_processing_for_request_id_without_metrics_agent
   end
